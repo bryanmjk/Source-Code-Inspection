d12eb61840084682d2543964790d5f1b8454c2e8
bryanmjk <31425503+bryanmjk@users.noreply.github.com>
2017-11-12 23:43:34 -0200

    Update TicketMachine.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -3,6 +3,8 @@ package br.calebe.ticketmachine.core;
 import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
 import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -13,40 +15,74 @@ public class TicketMachine {
     protected int valor;
     protected int saldo;
     protected int[] papelMoeda = {2, 5, 10, 20, 50, 100};
+    protected Scanner in = new Scanner(System.in);
 
     public TicketMachine(int valor) {
         this.valor = valor;
         this.saldo = 0;
     }
 
-    public void inserir(int quantia) throws PapelMoedaInvalidaException {
-        boolean achou = false;
+    public void inserir(int quantia){
+        try{
+            boolean achou = false;
+            
         for (int i = 0; i < papelMoeda.length && !achou; i++) {
             if (papelMoeda[1] == quantia) {
                 achou = true;
+                System.out.println("Valor aceito");
             }
         }
         if (!achou) {
             throw new PapelMoedaInvalidaException();
         }
         this.saldo += quantia;
+    } catch (PapelMoedaInvalidaException e) {
+            System.out.println("Nota recusada! Favor remover" + "Digite 1 para proseguir");
+            int removeu = in.nextInt();
+            
+            while (removeu != 1) {
+                System.out.println("Favor remover nota");
+                removeu = in.nextInt();
+            }
+        }
     }
 
     public int getSaldo() {
         return saldo;
     }
 
-    public Iterator<Integer> getTroco() {
-        return null;
+    public void zerarSaldo(){
+        this.saldo = 0;
     }
+    
+    public void getTroco(int saldo) {
+        Troco troco = new Troco(saldo);
+        List<PapelMoeda> papel = troco.getValorQtd();
+        for (PapelMoeda p : papel) {
+            if (p.getQuantidade() > 0) {
+                System.out.println("Valor da nota: " + p.getValor());
+                System.out.println("Quantidade de notas: " + p.getQuantidade());
+            }
+        }
+    } 
+
 
     public String imprimir() throws SaldoInsuficienteException {
-        if (saldo < valor) {
-            throw new SaldoInsuficienteException();
+        try{
+            if (saldo < valor) {
+                throw new SaldoInsuficienteException("Saldo insuficiente");
         }
+        this.saldo -= this.valor;
+            
         String result = "*****************\n";
+        result += "Remova seu ticket\n";
+        result += "*****************\n";
+        result += "Saldo restante:\n";
         result += "*** R$ " + saldo + ",00 ****\n";
         result += "*****************\n";
         return result;
-    }
+        } catch (SaldoInsuficienteException e) {
+            System.out.println(e.getMessage());
+            retorno null;
+        }
 }
